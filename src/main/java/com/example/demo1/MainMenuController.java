817de094227c6493b0b73c24fe917e0bdd1b7810
ryanp <ryanphillipsmusic41@gmail.com>
2022-04-02 01:51:37 -0400

    I'm styling the GUI using CSS. If the code doesn't run, try commenting out the customerAssignment() call in Main.main().

--- src/main/java/com/example/demo1/MainMenuController.java
+++ src/main/java/com/example/demo1/MainMenuController.java
@@ -8,10 +8,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,6 +27,8 @@ import java.util.ResourceBundle;
  * A series of ActionEvents used to switch between scenes via the main menu
  */
 public class MainMenuController implements Initializable {
+
+    private Popup helpPopup;
 
     @FXML
     private MenuBar exitMenu;
@@ -34,6 +41,7 @@ public class MainMenuController implements Initializable {
 
     @FXML
     void newAssociate(ActionEvent event) throws IOException {
+        if (helpPopup != null) { helpPopup.hide();}
         Parent root = FXMLLoader.load(getClass().getResource("NewAssociateInfo.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 600,575, Color.TRANSPARENT);
@@ -45,6 +53,7 @@ public class MainMenuController implements Initializable {
 
     @FXML
     void newCustomer(ActionEvent event) throws IOException {
+        if (helpPopup != null) { helpPopup.hide();}
         Parent root = FXMLLoader.load(getClass().getResource("NewCustomerInfo.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 600,400, Color.TRANSPARENT);
@@ -56,6 +65,7 @@ public class MainMenuController implements Initializable {
 
     @FXML
     void newProduct(ActionEvent event) throws IOException {
+        if (helpPopup != null) { helpPopup.hide();}
         Parent root = FXMLLoader.load(getClass().getResource("NewProduct.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 600,400, Color.TRANSPARENT);
@@ -67,6 +77,7 @@ public class MainMenuController implements Initializable {
 
     @FXML
     void customerRecords(ActionEvent event) throws IOException {
+        if (helpPopup != null) { helpPopup.hide();}
         Parent root = FXMLLoader.load(getClass().getResource("CustomerRecords.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 600,725, Color.TRANSPARENT);
@@ -79,6 +90,7 @@ public class MainMenuController implements Initializable {
 
     @FXML
     void productRecords(ActionEvent event) throws IOException {
+        if (helpPopup != null) { helpPopup.hide();}
         Parent root = FXMLLoader.load(getClass().getResource("ProductRecords.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 600,725, Color.TRANSPARENT);
@@ -91,6 +103,7 @@ public class MainMenuController implements Initializable {
 
     @FXML
     void associateRecords(ActionEvent event) throws IOException {
+        if (helpPopup != null) { helpPopup.hide();}
         Parent root = FXMLLoader.load(getClass().getResource("AssociateRecords.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 600,725, Color.TRANSPARENT);
@@ -103,6 +116,7 @@ public class MainMenuController implements Initializable {
 
     @FXML
     void removeAssociate(ActionEvent event) throws IOException {
+        if (helpPopup != null) { helpPopup.hide();}
         Parent root = FXMLLoader.load(getClass().getResource("RemoveAssociate.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 600,400, Color.TRANSPARENT);
@@ -114,6 +128,7 @@ public class MainMenuController implements Initializable {
 
     @FXML
     void removeCustomer(ActionEvent event) throws IOException {
+        if (helpPopup != null) { helpPopup.hide();}
         Parent root = FXMLLoader.load(getClass().getResource("RemoveCustomer.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 600,400, Color.TRANSPARENT);
@@ -125,6 +140,7 @@ public class MainMenuController implements Initializable {
 
     @FXML
     void removeProduct(ActionEvent event) throws IOException {
+        if (helpPopup != null) { helpPopup.hide();}
         Parent root = FXMLLoader.load(getClass().getResource("RemoveProduct.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 600,400, Color.TRANSPARENT);
@@ -132,6 +148,54 @@ public class MainMenuController implements Initializable {
         stage.setScene(scene);
         stage.setTitle("Product Removal Form");
         stage.show();
+    }
+
+    /**
+     * Switches to program settings
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    void settings(ActionEvent event) throws IOException {
+        if (helpPopup != null) { helpPopup.hide();}
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Settings.fxml"));
+        Parent mainCallWindowFXML = loader.load();
+        Stage thisStage = (Stage) myImageView.getScene().getWindow();
+        Scene mainCallWindow = new Scene (mainCallWindowFXML, 600, 400, Color.TRANSPARENT);
+        mainCallWindow.getStylesheets().add("style.css");
+        thisStage.setScene(mainCallWindow);
+    }
+
+    /**
+     * Displays information related to the current Scene without closing the current Stage
+     * @param event
+     */
+    @FXML
+    void helpDisplay(ActionEvent event) throws IOException {
+        if (helpPopup == null) {
+            Popup popup = new Popup();
+            popup.setX(100);
+            popup.setY(200);
+            Label label = new Label("The Farmhouse Company Data Manager software\n" +
+                    "provides ease of access to sensitive clientele and\n " +
+                    "employee information. It allows for effortless\n" +
+                    "storage and modification of company records.\n\n" +
+                    "Please speak to management regarding any questions\nor contact our developer team:\n\n");
+            label.setTextAlignment(TextAlignment.CENTER);
+            label.setTextFill(Paint.valueOf("white"));
+            label.setStyle("-fx-label-padding: 10 10 10 10");
+            Label label2 = new Label("        rphillip@mail.umw.edu\n" +
+                    "        jhewitt2@mail.umw.edu\n" +
+                    "        jvogtli@mail.umw.edu");
+            label2.setTextAlignment(TextAlignment.CENTER);
+            label2.setTextFill(Paint.valueOf("blue"));
+            label2.setStyle("-fx-label-padding: 30 0 20 60");
+
+            VBox vBox = new VBox(label, label2);
+            popup.getContent().add(vBox);
+            helpPopup = popup;
+            helpPopup.show(exitMenu.getScene().getWindow());
+        }
     }
 
     /**
--- src/main/java/com/example/demo1/RemoveCustomerController.java
+++ src/main/java/com/example/demo1/RemoveCustomerController.java
@@ -1,5 +1,6 @@
 package com.example.demo1;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,7 +8,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,8 +33,64 @@ public class RemoveCustomerController implements Initializable {
     private Stage stage;
     private Scene scene;
 
+    private Popup helpPopup;
+
+    @FXML
+    private MenuBar exitMenu;
+
+    @FXML
+    private ImageView myImageView;
+
     @FXML
     private ListView<String> customerList;
+
+    /**
+     * Switches to program settings
+     * @param event
+     * @throws IOException
+     */
+    @FXML
+    void settings(ActionEvent event) throws IOException {
+        if (helpPopup != null) { helpPopup.hide();}
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Settings.fxml"));
+        Parent mainCallWindowFXML = loader.load();
+        Stage thisStage = (Stage) myImageView.getScene().getWindow();
+        Scene mainCallWindow = new Scene (mainCallWindowFXML, 600, 400, Color.TRANSPARENT);
+        mainCallWindow.getStylesheets().add("style.css");
+        thisStage.setScene(mainCallWindow);
+    }
+
+    /**
+     * Displays information related to the current Scene without closing the current Stage
+     * @param event
+     */
+    @FXML
+    void helpDisplay(ActionEvent event) throws IOException {
+        if (helpPopup == null) {
+            Popup popup = new Popup();
+            popup.setX(100);
+            popup.setY(200);
+            Label label = new Label("The Farmhouse Company Data Manager software\n" +
+                    "provides ease of access to sensitive clientele and\n " +
+                    "employee information. It allows for effortless\n" +
+                    "storage and modification of company records.\n\n" +
+                    "Please speak to management regarding any questions\nor contact our developer team:\n\n");
+            label.setTextAlignment(TextAlignment.CENTER);
+            label.setTextFill(Paint.valueOf("white"));
+            label.setStyle("-fx-label-padding: 10 10 10 10");
+            Label label2 = new Label("        rphillip@mail.umw.edu\n" +
+                    "        jhewitt2@mail.umw.edu\n" +
+                    "        jvogtli@mail.umw.edu");
+            label2.setTextAlignment(TextAlignment.CENTER);
+            label2.setTextFill(Paint.valueOf("blue"));
+            label2.setStyle("-fx-label-padding: 30 0 20 60");
+
+            VBox vBox = new VBox(label, label2);
+            popup.getContent().add(vBox);
+            helpPopup = popup;
+            helpPopup.show(exitMenu.getScene().getWindow());
+        }
+    }
 
     /**
      * Returns to main menu
@@ -33,12 +99,13 @@ public class RemoveCustomerController implements Initializable {
      */
     @FXML
     void exit(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root, 600,400);
+        if (helpPopup != null) { helpPopup.hide();}
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
+        Parent mainCallWindowFXML = loader.load();
+        stage = (Stage) myImageView.getScene().getWindow();
+        scene = new Scene (mainCallWindowFXML, 600, 400, Color.TRANSPARENT);
+        scene.getStylesheets().add("style.css");
         stage.setScene(scene);
-        stage.setTitle("Main Menu");
-        stage.show();
     }
 
     /**
@@ -51,6 +118,15 @@ public class RemoveCustomerController implements Initializable {
         handleItemClicks();
         for (int i = 0; i < Main.getCustomers().size(); i++){
             customerList.getItems().add(Main.getCustomers().get(i).getName());
+        }
+        exitMenu.setOnMouseClicked(e -> Platform.exit());
+
+        try {
+            Image logo = new Image(String.valueOf(getClass().getResource("/Images/logo.png")));
+            myImageView.setImage(logo);
+            myImageView.setVisible(true);
+        } catch (Exception e) {
+            System.out.println("Image Not Found");
         }
     }
 
